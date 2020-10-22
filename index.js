dcd6ee12948cbac4af5ebaf6ed07a5eb175df44b
Rojas <DRojas@meridionalseguros.com.ar>
2020-10-22 01:19:03 -0300

    exercise 3.1 finished

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        id:1,
+        name:"Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Diego Rojas",
+        number: "1161394793"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Funca</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
